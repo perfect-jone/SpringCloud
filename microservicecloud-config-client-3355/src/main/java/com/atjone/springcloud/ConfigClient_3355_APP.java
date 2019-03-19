65d28bb13c2ae2b9c6da737a3320eeb214908b5a
jone <xiaodai@atjone.com>
2019-03-19 22:50:48 +0800

    add config-client service

---
+++ microservicecloud-config-client-3355/src/main/java/com/atjone/springcloud/ConfigClient_3355_APP.java
@@ -0,0 +1,12 @@
+package com.atjone.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ConfigClient_3355_APP {
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClient_3355_APP.class, args);
+	}
+}
